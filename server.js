05d7c0fa60b6ede0b142a5426becfe4deeb5a510
Dan Rusu <dan.rusu@rms.com>
2020-03-31 01:52:09 +0300

    added mysql endpoint + postman demo

--- server.js
+++ server.js
@@ -1,41 +1,63 @@
 const express = require('express'); 
 const app = express();
-const sql = require('mssql');
 const bodyParser = require('body-parser');
+
+const [ mssqlClient, mysqlClient ] = [ 'mssql', 'mysql' ].map(require);
 
 app.use(express.json());
 app.use(bodyParser.raw());
 app.use(bodyParser.text());
 
-// Start server and listen on http://localhost:1111/ by default.
-// Port can be passed as first CLI argument.
-const args = process.argv.slice();
-const port = args[2] ? args[2] : 1111;
+const returnMssqlQueryResult = async (sqlConfig, query, res) => {   
 
-const returnMssqlQueryResult = async (sqlConfig, query, res) => {
-
-    const { user, password, server, database } = sqlConfig;
-    const connectionString = `mssql://${user}:${password}@${server}/${database}`;
-
-    console.log(`sqlConfig: ${JSON.stringify(sqlConfig, null, 2)}`);
-    console.log(`connectionString: ${connectionString}`);
-    console.log(`Executing MSSQL query: \n"${query}"`);
+    console.log(
+        `\nmssqlConfig: ${JSON.stringify(sqlConfig, null, 2)}`    
+        + `\nquery: \n"${query}"`);
     
     try {
-        await sql.connect(connectionString);
-        
-        result = await sql.query(query)   
+        await mssqlClient.connect(sqlConfig);        
+        result = await mssqlClient.query(query);
         
         res.type('json');
         res.end(JSON.stringify(result)); // Result in JSON format
-       s
     }
     catch(err){
+        console.log(err);
         res.status(500).send(`Database error - ${err}`);
     }
     finally{
-        await sql.close();
+        await mssqlClient.close();
     }
+};
+
+const returnMysqlQueryResult = (sqlConfig, query, res) => {   
+
+    console.log(
+        `\nmysqlConfig: ${JSON.stringify(sqlConfig, null, 2)}`    
+        + `\nquery: \n"${query}"`);
+    
+    try {
+        const connection = mysqlClient.createConnection(sqlConfig);        
+        connection.connect(err => {
+            if (err) {
+                console.log(err);
+                res.status(500).send(`Database error - ${err}`);
+                return;
+            }
+            connection.query(query, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    res.status(500).send(`Database error - ${err}`);
+                    return;
+                };
+                res.type('json');
+                res.end(JSON.stringify(result)); // Result in JSON format
+            });
+        });               
+    }
+    catch(err){
+        
+    }    
 };
 
 // routes
@@ -45,8 +67,8 @@ app.post('/db-api/mssql', (req, res) => {
         return false;
     }
     else{
-        const { query, sqlConfig } = req.body;
-        returnMssqlQueryResult(sqlConfig, query, res);
+        const { query, mssqlConfig } = req.body;
+        returnMssqlQueryResult(mssqlConfig, query, res);
    }
 });
 
@@ -60,8 +82,22 @@ app.post('/db-api/query', (req, res) => {
    }
 });
 
+app.post('/db-api/mysql', (req, res) => {
+    if (req.headers['content-type'] != 'application/json'){ 
+        res.status(500).send('Only JSON is supported!');
+        return false;
+    }
+    else{
+        const { query, mysqlConfig } = req.body;
+        returnMysqlQueryResult(mysqlConfig, query, res);
+   }
+});
 
-// start server
+// Start server and listen on http://localhost:1111/ by default.
+// Port can be passed as first CLI argument.
+const args = process.argv.slice(2);
+const port = args[0] ? args[0] : 1111;
+
 const server = app.listen(port, () =>
     console.log(`db-api server listening at http://localhost:${port}/db-api`)
 );
