9e8f1792a1c5c16f2da0bd65927cb86102396565
Dan Rusu <dan.rusu@rms.com>
2020-04-11 01:15:45 +0300

    renamed endpoints; update postman collection

--- app.js
+++ app.js
@@ -14,22 +14,15 @@ app.use(express.json());
 app.use(bodyParser.raw());
 app.use(bodyParser.text());
 
-const home = (req, res) => {
+const home = (_, res) => {
     const postmanUrl = 
-        `https://github.com/danrusu/db-api/blob/master/postman/db-api.postman_collection.json`
+        `https://github.com/danrusu/rest-sql/blob/master/postman/rest-sql.postman_collection.json`
     const homeText = `<h3>This is a back-end REST project.</h3>`
         + `<br> For usage info check this <a href="${postmanUrl}">Postman collection<a>.`;
     res.send(homeText);
 }
 
-// routes
-app.get('/', home);
-app.post('/db-api/mssql', mssqlClient.requestHandler);
-app.post('/db-api/mysql', mysqlClient.requestHandler);
-app.post('/db-api/postgres', postgresClient.requestHandler);
-
-
-app.post('/db-api/query', (req, res) => {
+const query = (req, res) => {
     if (req.headers['content-type'] != 'text/plain'){ 
         res.status(500).send('Only TEXT is supported!');
         return false;
@@ -37,11 +30,20 @@ app.post('/db-api/query', (req, res) => {
     else{
         res.send(req.body);
    }
-});
+};
+
+// routes
+app.get('/', home);
+// sql
+app.post('/mssql', mssqlClient.requestHandler);
+app.post('/mysql', mysqlClient.requestHandler);
+app.post('/postgres', postgresClient.requestHandler);
+
+app.post('/query', query);
 
 // Start server and listen on http://localhost:1111/ by default.
 const port = process.env.PORT || 1111;
 
 app.listen(port, () =>
-    console.log(`db-api server listening at http://localhost:${port}/db-api`)
+    console.log(`db-api server listening at http://localhost:${port}/`)
 );
